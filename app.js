ab75e163bd20e7a1124cec2674d5f26e568f8f42
ivolevy <ivo.levy03@gmail.com>
2021-09-28 10:42:15 -0300

    changes

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ app.get('/', (req,res) =>{
 app.get('/register', (req,res) =>{
     res.sendFile(path.resolve(__dirname, './views/register.html'));
 })
-app.get('/register', (req,res) =>{
-    res.send(path.resolve('ya te registraste!'));
-})
+app.post('/register', (req,res)=> {
+    res.send('Ya te registraste campeón');
+});
 
 app.get('/login', (req,res) =>{
     res.sendFile(path.resolve(__dirname, './views/login.html'));
 })
-app.get('/login', (req,res) =>{
+app.post('/login', (req,res) =>{
     res.send(path.resolve('Ya te logueaste!'));
 })
 
